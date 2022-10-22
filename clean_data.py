bfcf21d3f890c6af4ff15a24b310c3c9961c4db2
efugikawa <elizabeth.fugikawa@gmail.com>
2022-10-21 19:13:44 -0600

    change some columns

--- clean_data.py
+++ clean_data.py
@@ -30,17 +30,14 @@ class Main:
     def clean_data(self):
         logging.info(f"loading dataframe")
         # select only columns of interest
-        self.df_numeric = self.df[['caseid_new', 'ppagecat', 'ppincimp']].rename({'caseid_new': 'id',
-                                                                                  'ppagecat': 'age',
-                                                                                  'ppincimp': 'income'}, axis=1)
-        self.df_categorical = self.df[['ppgender', 'ppeducat', 'ppwork',
-                                       'pppartyid3', 'ppreg9',
-                                       'ppmarit', 'q24_met_online',
+        self.df_numeric = self.df[['caseid_new', 'ppage', 'hhinc']].rename(
+            {'caseid_new': 'id', 'ppage': 'age', 'hhinc': 'income'}, axis=1)
+        self.df_categorical = self.df[['ppgender', 'ppagecat', 'ppeducat', 'ppincimp', 'ppwork',
+                                       'pppartyid3', 'ppreg9', 'ppmarit', 'q24_met_online',
                                        'relationship_quality']].rename(
-            columns={'ppgender': 'gender', 'ppeducat': 'educ',
+            columns={'ppgender': 'gender', 'ppagecat':'agecat', 'ppeducat': 'educ', 'ppincimp':"incomecat",
                      'ppwork': 'job_status', 'pppartyid3': 'political_aff', 'ppreg9': 'region',
-                     'ppmarit': 'marital_status',
-                     'q24_met_online': 'met_online'})
+                     'ppmarit': 'marital_status', 'q24_met_online': 'met_online'})
         self.df_numeric_encoded = pd.get_dummies(self.df_numeric)
         self.df_categorical_encoded = pd.get_dummies(self.df_categorical)
         # print(self.df_categorical_encoded.head())
@@ -51,16 +48,17 @@ class Main:
         # print(self.df_categorical.isnull().sum())
         # print(self.df_numeric.isnull().sum())
 
-    def viz(self):
+    def gender(self):
         # Visualize gender representative
         female_count = self.df_categorical_encoded['gender_female'].value_counts()[
             self.df_categorical_encoded['gender_female'] == 1].values[0]
         male_count = self.df_categorical_encoded['gender_female'].value_counts()[
             self.df_categorical_encoded['gender_female'] == 1].values[1]
         gender = pd.DataFrame({'gender': ['female', 'male'], 'count': [female_count, male_count]})
-        sns.barplot(x='gender', y='count', data=gender, palette="hls")
+        sns.barplot(x='gender', y='count', data=gender, palette='hls')
         plt.show()
 
+    def political(self):
         # Visualize political representative
         democrat_count = self.df_categorical_encoded['political_aff_democrat'].value_counts()[1]
         republican_count = self.df_categorical_encoded['political_aff_republican'].value_counts()[1]
@@ -77,17 +75,42 @@ class Main:
     def pivot(self):
         logging.info("creating pivot tables")
         full_df = pd.concat([self.df_numeric, self.df_categorical], axis=1)
-        t1 = full_df.pivot_table(values=["id"], index=["region", "income"], aggfunc='count')
+        t1 = full_df.pivot_table(values=["income"], index=["region"], aggfunc=np.mean)
         # print(t1)
         t2 = full_df.pivot_table(values=["id"], index=["marital_status", "met_online"], aggfunc='count')
         # print(t2)
         t3 = full_df.pivot_table(values=["id"], index=["political_aff", "age"], aggfunc='count')
-        print(t3)
+        print(t1)
+
+        # interesting question, what season or month did you meet your significant other?
+        # TODO: viz of map/region, pull month met data, pairpolt, (ggqqplot) normalize plot for numeric values
+        # income, pivot_tables, regplot, avg age vs income, missing data?
+
+
+    def age(self):
+        age_col_df = self.df_numeric_encoded.iloc[2:, :8]
+        age_col_df.drop(labels='id', axis=1, inplace=True)
+        count_df = pd.DataFrame({'count':  age_col_df.sum()})
+        count_df.rename(index={'cat_age_18-24':'18-24','cat_age_25-34':'25-34',
+                               'cat_age_35-44':'35-44','cat_age_45-54':'45-54',
+                               'cat_age_55-64':'55-64','cat_age_65-74':'65-74',
+                               'cat_age_75+':'75+'}, inplace=True)
+        count_df.reset_index(inplace=True)
+        count_df.rename(columns={'index': 'age'}, inplace=True)
+        sns.barplot(x='age', y='count', data=count_df, palette='hls')
+        plt.show()
+
+    def pair(self):
+        sns.pairplot(self.df_numeric_encoded)
 
 
 if __name__ == '__main__':
     m = Main()
     m.log_config()
     m.clean_data()
-    m.viz()
+    m.gender()
+    m.political()
+    m.pivot()
+    m.age()
+    # m.pair()
     m.pivot()
