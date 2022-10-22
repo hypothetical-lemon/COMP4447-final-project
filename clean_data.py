e81ae7b4dcc2cef6b1155c0949eeb8309b138eb6
Heather Lemon <mcmillenh90@gmail.com>
2022-10-21 18:04:42 -0600

    adds age graph, reset requirements, adds png, update notebook

--- clean_data.py
+++ clean_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import logging
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.api as sm
 
 class Main:
 
@@ -28,8 +29,8 @@ class Main:
     def clean_data(self):
         logging.info(f"loading dataframe")
         # select only columns of interest
-        self.df_numeric = self.df[['caseid_new', 'ppagecat', 'ppincimp']].rename({'caseid_new':'id',
-                                                                                  'ppagecat':'age',
+        self.df_numeric = self.df[['caseid_new', 'ppage', 'ppagecat', 'ppincimp']].rename({'caseid_new':'id', 'ppage':'age',
+                                                                                  'ppagecat':'cat_age',
                                                                                   'ppincimp':'income'}, axis=1)
         self.df_categorical = self.df[['ppgender', 'ppeducat', 'ppwork',
                                        'pppartyid3', 'ppreg9',
@@ -48,29 +49,57 @@ class Main:
         print(self.df_numeric.isnull().sum())
 
 
-    def viz(self):
+    def gender(self):
         # Visualize gender representative
         female_count = self.df_categorical_encoded['gender_female'].value_counts()[self.df_categorical_encoded['gender_female']==1].values[0]
         male_count = self.df_categorical_encoded['gender_female'].value_counts()[self.df_categorical_encoded['gender_female']==1].values[1]
         gender = pd.DataFrame({'gender': ['female', 'male'], 'count': [female_count, male_count]})
-        sns.barplot(x='gender', y='count', data=gender, hue='gender')
+        sns.barplot(x='gender', y='count', data=gender, palette='hls')
         plt.show()
 
+    def political(self):
         # Visualize political representative
         democrat_count = self.df_categorical_encoded['political_aff_democrat'].value_counts()[1]
         republican_count = self.df_categorical_encoded['political_aff_republican'].value_counts()[1]
         other_count = self.df_categorical_encoded['political_aff_other'].value_counts()[1]
         party_aff = pd.DataFrame({'political_party': ['democrat', 'republican', 'other'], 'count': [democrat_count, republican_count, other_count]})
-        sns.barplot(x='political_party', y='count', data=party_aff, hue='political_party')
+        sns.barplot(x='political_party', y='count', data=party_aff,  palette='hls')
         plt.xlabel('Political Party Affiliation')
         plt.ylabel('Count')
         plt.title('Political Party Affiliation Representation')
         plt.legend()
         plt.show()
 
+        # interesting question, what season or month did you meet your significant other?
+        #TODO: viz of map/region, pull month met data, pairpolt, (ggqqplot) normalize plot for numeric values
+        # income, pivot_tables, regplot, avg age vs income, missing data?
+
+    def age(self):
+        age_col_df = self.df_numeric_encoded.iloc[2:, :9]
+        age_col_df.drop(labels='id', axis=1, inplace=True)
+        age_col_df.drop(labels='age', axis=1, inplace=True)
+        count_df = pd.DataFrame({'count':  age_col_df.sum()})
+        count_df.rename(index={'cat_age_18-24':'18-24','cat_age_25-34':'25-34',
+                               'cat_age_35-44':'35-44','cat_age_45-54':'45-54',
+                               'cat_age_55-64':'55-64','cat_age_65-74':'65-74',
+                               'cat_age_75+':'75+'}, inplace=True)
+        count_df.reset_index(inplace=True)
+        count_df.rename(columns={'index': 'age'}, inplace=True)
+        sns.barplot(x='age', y='count', data=count_df, palette='hls')
+        plt.show()
+
+
+    def pair(self):
+        sns.pairplot(self.df_numeric_encoded)
+
+
+
 
 if __name__ == '__main__':
     m = Main()
     m.log_config()
     m.clean_data()
-    m.viz()
+    m.gender()
+    m.poltical()
+    m.age()
+    # m.pair()
