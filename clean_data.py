2a296ced82cdefaf8bceeb1e2ff0b38f8e5c3431
Heather Lemon <mcmillenh90@gmail.com>
2022-10-30 17:00:27 -0600

    update intro text, add dta file, add OLS and shaprio (#10)
    * Update notebook intro text
    
    * add dta file
    
    * add OLS and shaprio

--- clean_data.py
+++ clean_data.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import statsmodels.api as sm
-
+import scipy
 pd.options.display.max_columns = None
-
+import warnings
+warnings.filterwarnings("ignore")
 class Main:
 
     def __init__(self):
@@ -45,18 +46,26 @@ class Main:
     def clean_data(self):
         logging.debug("paring down full dataframe into relevant columns")
         # select only columns of interest
-        self.df_numeric = self.full_df[['CASEID_NEW', 'ppage', 'ppagecat', 'hhinc']].rename(
-            {'CASEID_NEW': 'id', 'ppage': 'age', 'ppagecat': 'cat_age', 'papreligion': 'religion'}, axis=1)
+        self.df_numeric = self.full_df[['ppage', 'ppagecat', 'hhinc']].rename(
+            {'ppage': 'age', 'ppagecat': 'cat_age'}, axis=1)
 
-        self.df_categorical = self.full_df[['ppgender', 'ppeducat', 'ppincimp', 'ppwork',
-                                       'pppartyid3', 'ppreg9', 'ppmarit', 'q24_met_online', 'papreligion',
-                                       'relationship_quality']].rename(
-            columns={'ppgender': 'gender', 'ppagecat': 'agecat', 'ppeducat': 'educ', 'ppincimp': "incomecat",
-                     'ppwork': 'job_status', 'pppartyid3': 'political_aff', 'ppreg9': 'region',
-                     'papreligion': 'religion', 'w6_otherdate_app_2': 'app_used',
-                     'ppmarit': 'marital_status', 'q24_met_online': 'met_online'})
-
+        self.df_categorical = self.full_df[['ppgender', 'ppeducat', 'ppincimp',
+                                            'ppwork', 'pppartyid3', 'ppreg9',
+                                            'ppmarit', 'q24_met_online',
+                                            'papreligion', 'relationship_quality']].rename(
+            columns={'ppgender': 'gender',
+                     'ppeducat': 'educ',
+                     'ppincimp': "incomecat",
+                     'ppwork': 'job_status',
+                     'pppartyid3': 'political_aff',
+                     'ppreg9': 'region',
+                     'papreligion': 'religion',
+                     'w6_otherdate_app_2': 'app_used',
+                     'ppmarit': 'marital_status',
+                     'q24_met_online': 'met_online'})
+        # self.df_numeric.fillna(0, inplace=True)
         self.df_numeric['hhinc'] = self.df_numeric['hhinc'].astype(int)
+        # self.df_numeric['age'] = self.df_numeric['age'].astype(int)
         self.df_numeric_encoded = pd.get_dummies(self.df_numeric)
         self.df_categorical_encoded = pd.get_dummies(self.df_categorical)
         # print(self.df_categorical_encoded.head())
@@ -120,7 +129,7 @@ class Main:
         sns.barplot(x='age', y='count', data=count_df, palette='hls')
         plt.show()
 
-    def pair(self):
+    def relplot(self):
         """
         plot pairwise relationships
         """
@@ -128,15 +137,32 @@ class Main:
         # sns.pairplot(self.df_numeric, x_vars=['age'], y_vars=['hhinc'], palette='hls', hue='hhinc', height=5)
         plt.show()
 
+    def ols(self):
+        model = sm.OLS(self.df_categorical_encoded['met_online_met offline'], self.df_numeric['hhinc'])
+        results = model.fit()
+        print(results.params)
+        print(results.summary())
+
+    def shapiro(self):
+        np.random.seed(1)
+        print(scipy.stats.shapiro(self.df_categorical_encoded['political_aff_democrat']))
+        print(scipy.stats.shapiro(self.df_categorical_encoded['political_aff_republican']))
+        print(scipy.stats.shapiro(self.df_categorical_encoded['political_aff_other'].sample(n=500)))
+
+    def qqplot(self):
+        sm.qqplot(data=self.df_categorical['political_aff'], line='45')
+        plt.show()
 
 if __name__ == '__main__':
     m = Main()
     m.log_config()
     m.clean_data()
+    # general stats EDA
     m.gender()
     m.political()
-    m.pivot()
-    m.age()
-    # sm.MANOVA
-    # sm.OLS()
-    m.pair()
+    # m.pivot()
+    # m.age()
+    # m.pair()
+    m.ols()
+    m.shapiro()
+    m.qqplot()
